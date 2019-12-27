6fa066c519e6d130826b15d4d378107bf7f0a8c0
Hari Kammana <hari.kammana@gmail.com>
2019-12-27 10:44:37 +0530

    fixed syntax and dependency issues

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-form flask import Flask
+from flask import Flask
 from redis import Redis
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    count = redis.incr('hits')
+    #count = redis.incr('hits')
     return '<h1 style="color:red">Welcome to Java Home Docker App</h1>'
 
 if __name__ == "__main__":
