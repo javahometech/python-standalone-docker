bac0b252ce33f8cc7d04b7118d7ef7d87bb3c5b3
javahometech <kammana.hari@gmail.com>
2019-12-27 10:39:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+form flask import Flask
+from redis import Redis
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return '<h1 style="color:red">Welcome to Java Home Docker App</h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
